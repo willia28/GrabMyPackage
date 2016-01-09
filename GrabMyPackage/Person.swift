e07a6ca5b29d3f1726125ad8d38d1e0f450b0d31
Nicholas Galasso <nick.galasso@animoto.com>
2016-01-09 15:22:22 -0500

    fix stuff

---
+++ GrabMyPackage/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  GrabMyPackage
+//
+//  Created by Nicholas Galasso on 1/9/16.
+//  Copyright © 2016 Matt Williamson. All rights reserved.
+//
+// Defining a person
+import Foundation
+
+struct Person  {
+    var firstName:String?
+    var lastName:String?
+    var emailAddress:String?
+}
+
+
